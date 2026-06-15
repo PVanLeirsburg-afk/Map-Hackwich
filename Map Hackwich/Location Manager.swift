578cbc585c7236e3a783a53a87fdabcf6fe59cd9
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-15 13:07:49 -0500

    Added a location to the map

--- Map Hackwich/Location Manager.swift
+++ Map Hackwich/Location Manager.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import CoreLocation
+
+@Observable
+class LocationManager: NSObject, CLLocationManagerDelegate {
+   var locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+}
